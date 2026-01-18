5952c98e5d88b6f2da482d28e1b9e92bb486bb32
Andrej <pytpoint@gmail.com>
2026-01-18 11:11:12 +0100

    pr

--- salary.py
+++ salary.py
@@ -1,22 +1,36 @@
-def total_salary(path):
-    total = 0
-    count = 0
+# def total_salary(path):
+#     total = 0
+#     count = 0
 
-    try:
-        with open(path, "r", encoding="utf-8") as file:
-            for line in file:
-                name, salary = line.strip().split(",")
-                total += int(salary)
-                count += 1
+#     try:
+#         with open(path, "r", encoding="utf-8") as file:
+#             for line in file:
+#                 name, salary = line.strip().split(",")
+#                 total += int(salary)
+#                 count += 1
 
-        if count != 0:
-            average = total / count
-        else:
-            average = 0
-        return total, average
+#         if count != 0:
+#             average = total / count
+#         else:
+#             average = 0
+#         return total, average
 
-    except FileNotFoundError:
-        return 0, 0
+#     except FileNotFoundError:
+#         return 0, 0
     
-x = total_salary("file.txt")
-print(type(x))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
