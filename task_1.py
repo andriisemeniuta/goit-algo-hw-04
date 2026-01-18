9ffa097b6627c77d0fb0821ccee272a87b3ea502
Andrej <pytpoint@gmail.com>
2026-01-18 12:29:07 +0100

    rr

---
+++ task_1.py
@@ -0,0 +1,62 @@
+def total_salary(path):
+    total = 0
+    count = 0
+    
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            for line in file:
+                name, salary = line.strip().split(",")
+                total += int(salary)
+                count += 1
+                
+        if count != 0:
+            average = total / count
+        else:
+            average = 0
+        return total, average
+        
+    except FileNotFoundError:
+        return 0, 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
